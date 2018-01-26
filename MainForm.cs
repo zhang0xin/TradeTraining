745e0b1b85832213389e0028cd42b127a74c4e3e
zhangyanxin <zhang0xin@gmail.com>
2018-01-26 16:13:57 +0800

    Initial commit

---
+++ MainForm.cs
@@ -0,0 +1,13 @@
+using System.Windows.Forms;
+
+namespace TradeTraining
+{
+	class MainForm : Form
+	{
+		Panel canvas = new Panel();
+		public MainForm()
+		{
+			Controls.Add(canvas);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+using System.Windows.Forms;
+namespace TradeTraining
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Application.Run(new MainForm());
+		}
+	}
+}
