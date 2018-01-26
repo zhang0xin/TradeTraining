3fb2c802a5a6600e946c9132421d0715de079e2d
zhangyanxin <zhang0xin@gmail.com>
2018-01-26 16:41:39 +0800

    init

--- MainForm.cs
+++ MainForm.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace TradeTraining
@@ -7,6 +9,11 @@ namespace TradeTraining
 		Panel canvas = new Panel();
 		public MainForm()
 		{
+			canvas.BackColor = Color.White;
+			canvas.Paint += delegate(object sender, PaintEventArgs e)
+			{
+				e.Graphics.DrawLine(new Pen(Color.Black, 3), 0, 0, 90, 90);
+			};
 			Controls.Add(canvas);
 		}
 	}
